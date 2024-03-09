c94d19c6b5f9207c3cf29f7795cafbb46a167fc9
LuisaPaez27 <158387131+LuisaPaez27@users.noreply.github.com>
2024-03-08 22:10:31 -0500

    Delete ConfiguracionHoraria.java

--- ConfiguracionHoraria.java
+++
@@ -1,9 +0,0 @@
-package PrincipioLiskov;
-
-public class ConfiguracionHoraria implements ILoadRecursoPersistente{
-    @Override
-    public void load() {
-        System.out.println("Configuracion horaria cargada");
-    }
-
-}
